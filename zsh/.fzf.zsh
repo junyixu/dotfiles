ec7560f553477c71cb915ffe581370bfd4929ee8
jy0x0 <junyixu0@gmail.com>
2021-10-10 11:03:43 +0800

    update

--- zsh/.fzf.zsh
+++ zsh/.fzf.zsh
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
 if [[ ! "$PATH" == */home/junyi/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/junyi/.fzf/bin"
+  export PATH="${PATH:+${PATH}:}$HOME/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/junyi/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/junyi/.fzf/shell/key-bindings.zsh"
+source "$HOME/.fzf/shell/key-bindings.zsh"
