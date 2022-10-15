a2a77df6de224d067501290c11f68819c15bb764
Junyi Xu <junyixu0@gmail.com>
2022-10-15 22:24:18 +0800

    misc

---
+++ MyShell/bin/commit.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+#
+# commit.sh
+# git commit 
+# overleaf & dropbox
+# Copyright (C) 2022 junyi <junyi@Junix>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+cd /home/junyi/Dropbox/应用/Overleaf/trueGraduationProject && git merge main && cd -
