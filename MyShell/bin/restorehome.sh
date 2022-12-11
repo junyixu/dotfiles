641709be3603ade183a45932948f8bee5ffe7f08
Junyi Xu <junyixu0@gmail.com>
2022-12-11 13:08:28 +0800

    已经在 /etc/fstab 中自动挂载

--- MyShell/bin/restorehome.sh
+++ MyShell/bin/restorehome.sh
@@ -7,6 +7,5 @@
 mountDirector='/mnt/theRoot'
 lastID=$(sudo snapper --config home list | tail -1 | cut -d' ' -f1)
 
-sudo mount -o subvol=/ /dev/nvme0n1p2 $mountDirector
 sudo btrfs subvolume snapshot $mountDirector/@home/.snapshots/$lastID/snapshot $mountDirector/home_bak
 echo 'Created a snapshot successfully!'
