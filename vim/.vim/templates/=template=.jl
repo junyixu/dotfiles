d2a936b1703d8c0c5e9f255b0a8b5f988d67743e
JunyiXu@ArchSurface <junyixu0@gmail.com>
2023-10-12 18:15:54 +0800

    update

---
+++ vim/.vim/templates/=template=.jl
@@ -0,0 +1,11 @@
+#! /usr/bin/env julia
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#=
+    %FILE%
+    Copyright © %YEAR% %USER% <%MAIL%>
+
+    Distributed under terms of the %LICENSE% license.
+=#
+
+%HERE%
